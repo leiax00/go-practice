f45d5ca0b7a3b869bbe962fc4bde45509573d94f
leiax00 <leiax00@outlook.com>
2021-06-21 09:17:50 +0800

    提交redis benchmark

--- benchmark-redis/readme.md
+++ benchmark-redis/readme.md
@@ -1,2 +1,38 @@
 # benchmark-redis
 > For benchmark redis
+
+1. 使用 redis benchmark 工具, 测试 10 20 50 100 200 1k 5k 字节 value 大小，redis get set 性能。
+
+参见: `redis_test.go`
+测试结果:
+begin
+goos: windows
+goarch: amd64
+pkg: benchmark-redis
+cpu: AMD Ryzen 7 5800H with Radeon Graphics         
+BenchmarkRedisKv_10b
+BenchmarkRedisKv_10b-16     	   34886	     35178 ns/op
+BenchmarkRedisKv_20b
+BenchmarkRedisKv_20b-16     	   34065	     34383 ns/op
+BenchmarkRedisKv_50b
+BenchmarkRedisKv_50b-16     	   32989	     35281 ns/op
+BenchmarkRedisKv_100b
+BenchmarkRedisKv_100b-16    	   34002	     35685 ns/op
+BenchmarkRedisKv_200b
+BenchmarkRedisKv_200b-16    	   32908	     35705 ns/op
+BenchmarkRedisKv_1k
+BenchmarkRedisKv_1k-16      	   33211	     36843 ns/op
+BenchmarkRedisKv_5k
+BenchmarkRedisKv_5k-16      	   28593	     40895 ns/op
+PASS
+end
+
+单个key的get和set性能差距不大;
+
+
+2、写入一定量的 kv 数据, 根据数据大小 1w-50w 自己评估, 结合写入前后的 info memory 信息  , 分析上述不同 value 大小下，平均每个 key 的占用内存空间。
+1w key:
+         10                 20              50              100             200         1000            5000
+之前      1672408           1119960         1612336         1120912          1122472     1121648        1121648
+之后      1970248           2051200         2371200         2931200          4051728     12051936       52991016
+avg      29.78             93.124           75.89           181.03          292.93      1093.03         5186.94
--- benchmark-redis/redis_test.go
+++ benchmark-redis/redis_test.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"github.com/go-redis/redis/v8"
+	"math/rand"
 	"testing"
+	"time"
 )
 
 var rdb *redis.Client
@@ -18,43 +20,57 @@ func TestMain(m *testing.M) {
 }
 
 func initData() {
-	//cnt := getClient()
-	//cnt.Set(ctx, )
+	cnt := getClient()
+	params := []int{10, 20, 50, 100, 200, 1000, 5000}
+	for _, item := range params {
+		cnt.Set(ctx, fmt.Sprintf("key_%d", item), getRandomString(item), 0)
+	}
+}
+
+func getRandomString(l int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyz"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
 }
 
 func BenchmarkRedisKv_10b(b *testing.B) {
-	b.Log("start to test for 10 bytes value...")
-	getRedisKv(b, "key_10b")
+	//b.Log("start to test for 10 bytes value...")
+	getRedisKv(b, "key_10")
 }
 
 func BenchmarkRedisKv_20b(b *testing.B) {
-	b.Log("start to test for 20 bytes value...")
-	getRedisKv(b, "key_20b")
+	//b.Log("start to test for 20 bytes value...")
+	getRedisKv(b, "key_20")
 }
 
 func BenchmarkRedisKv_50b(b *testing.B) {
-	b.Log("start to test for 50 bytes value...")
-	getRedisKv(b, "key_50b")
+	//b.Log("start to test for 50 bytes value...")
+	getRedisKv(b, "key_50")
 }
 
 func BenchmarkRedisKv_100b(b *testing.B) {
-	b.Log("start to test for 100 bytes value...")
-	getRedisKv(b, "key_100b")
+	//b.Log("start to test for 100 bytes value...")
+	getRedisKv(b, "key_100")
 }
 
 func BenchmarkRedisKv_200b(b *testing.B) {
-	b.Log("start to test for 200 bytes value...")
-	getRedisKv(b, "key_200b")
+	//b.Log("start to test for 200 bytes value...")
+	getRedisKv(b, "key_200")
 }
 
 func BenchmarkRedisKv_1k(b *testing.B) {
-	b.Log("start to test for 1 kb value...")
-	getRedisKv(b, "key_1k")
+	//b.Log("start to test for 1 kb value...")
+	getRedisKv(b, "key_1000")
 }
 
 func BenchmarkRedisKv_5k(b *testing.B) {
-	b.Log("start to test for 5 kb value...")
-	getRedisKv(b, "key_5k")
+	//b.Log("start to test for 5 kb value...")
+	getRedisKv(b, "key_5000")
 }
 
 func getRedisKv(b *testing.B, key string) {
@@ -62,8 +78,8 @@ func getRedisKv(b *testing.B, key string) {
 	client := getClient()
 	b.StartTimer()
 	for i := 1; i < b.N; i++ {
-		val := client.Get(ctx, key).Val()
-		b.Log(val)
+		_ = client.Get(ctx, key).Val()
+		//b.Log(val)
 	}
 }
 
@@ -77,4 +93,15 @@ func getClient() *redis.Client {
 
 	}
 	return rdb
+}
+
+func TestCalcAvgMemory(t *testing.T) {
+	cnt := getClient()
+	params := []int{10, 20, 50, 100, 200, 1000, 5000}
+	for _, item := range params {
+		cnt.FlushDB(ctx)
+		for i := 0; i < 10000; i++ {
+			cnt.Set(ctx, fmt.Sprintf("key_%d", i), getRandomString(item), 0)
+		}
+	}
 }
